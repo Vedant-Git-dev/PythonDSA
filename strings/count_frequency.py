73c8dd8b66c2505e8bec5d27716420a2f3f2ca62
Vedant Pardeshi <vedant.pardeshi@ritppune.com>
2025-09-03 12:13:42 +0530

    Updated count_frequency.py

--- strings/count_frequency.py
+++ strings/count_frequency.py
@@ -1,10 +1,11 @@
 def count_occurance(str, ch):
     count = 0
-    for char in str:
-        if ch == char:
+    for char in str.lower():
+        if ch.lower() == char:
             count += 1
 
     return count
+
 
 
 print(count_occurance('hello world', "l"))
