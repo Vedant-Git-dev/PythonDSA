d6860cd6f61649fc59800cab1bafbd9eb8dd0f35
Vedant Pardeshi <vedant.pardeshi@ritppune.com>
2025-09-03 12:15:46 +0530

    Updated check_palindrome.py

--- strings/check_palindrome.py
+++ strings/check_palindrome.py
@@ -2,7 +2,7 @@ def check_palindrome(str):
     i = 0
     j = len(str)-1
     while i < j:
-        if str[i] != str[j]:
+        if str[i].lower() != str[j].lower():
             return "Not a palindrome"
 
         i += 1
@@ -11,4 +11,5 @@ def check_palindrome(str):
     return "Palindrome"
 
 print(check_palindrome('hello'))
+
 print(check_palindrome('abccba'))
